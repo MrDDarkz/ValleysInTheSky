0d15711ccd11208895d48cb507b595bca723dc9b
DDarkz <fuji.wajana2@gmail.com>
2025-01-02 21:42:29 +0700

    Initial commit

---
+++ src/main/java/ddarkz/vits/ValleysInTheSkyClient.java
@@ -0,0 +1,10 @@
+package ddarkz.vits;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ValleysInTheSkyClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/ddarkz/vits/ValleysInTheSkyDataGenerator.java
@@ -0,0 +1,11 @@
+package ddarkz.vits;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ValleysInTheSkyDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
