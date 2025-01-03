4a2869e80024db0ee41b783db38ccaa9ee5e4dd5
DDarkz <fuji.wajana2@gmail.com>
2025-01-03 23:18:27 +0700

    Added 3 new items, got textures and item registration to work. Added the valley_caller block, still working no registration. Added logging.

--- src/main/java/ddarkz/vits/block/ModBlocks.java
+++ src/main/java/ddarkz/vits/block/ModBlocks.java
@@ -2,22 +2,23 @@ package ddarkz.vits.block;
 
 import ddarkz.vits.ValleysInTheSky;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-import net.minecraft.block.*;
-import net.minecraft.block.enums.NoteBlockInstrument;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
-import net.minecraft.registry.*;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
-
-import java.util.function.Function;
 
 public class ModBlocks {
     public static Block register(Block block, String name, boolean shouldRegisterItem) {
         Identifier id = Identifier.of(ValleysInTheSky.MOD_ID, name);
         RegistryKey<Block> key = RegistryKey.of(RegistryKeys.BLOCK, id);
-        Block.Settings settings = ((Block.Settings) block.getSettings()).registryKey(key);
+        Block.Settings settings = block.getSettings().registryKey(key);
         if (shouldRegisterItem) {
             BlockItem blockItem = new BlockItem(block, new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, id)));
             Registry.register(Registries.ITEM, id, blockItem);
